710a07b85d64a44de0a743e24491f00d430867f4
theWazinator <jacobabrown999@gmail.com>
2022-04-08 17:21:02 -0400

    Backup with first try of separating dates in the for loop.

---
+++ Convert_To_ML_Helper_Methods.py
@@ -0,0 +1,243 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+def create_ML_features(df):
+
+    columns_to_keep = [
+        'average_matchrate',
+        'untagged_controls',
+        'untagged_response',
+        'passed_liveness',
+        'connect_error',
+        'in_control_group',
+        'excluded_below_threshold',
+        'delta_time',
+        'control_response_start_success',
+        'control_response_end_success',
+        'control_response_start_has_type_a',
+        'control_response_start_rcode',
+        'control_response_end_has_type_a',
+        'control_response_end_rcode',
+        'test_query_successful',
+        'test_query_unsuccessful_attempts',
+        'test_noresponse_1_has_type_a',
+        'test_noresponse_1_rcode',
+        'test_noresponse_2_has_type_a',
+        'test_noresponse_2_rcode',
+        'test_noresponse_3_has_type_a',
+        'test_noresponse_3_rcode',
+        'test_noresponse_4_has_type_a',
+        'test_noresponse_4_rcode',
+        'test_response_has_type_a',
+        'test_response_rcode',
+        'test_response_IP_count',
+        'test_response_0_IP_match',
+        'test_response_0_http_match',
+        'test_response_0_cert_match',
+        'test_response_0_asnum_match',
+        'test_response_0_asname_match',
+        'test_response_0_match_percentage',
+        'test_response_0_asnum',
+        'test_response_1_IP_match',
+        'test_response_1_http_match',
+        'test_response_1_cert_match',
+        'test_response_1_asnum_match',
+        'test_response_1_asname_match',
+        'test_response_1_match_percentage',
+        'test_response_1_asnum',
+        'test_response_2_IP_match',
+        'test_response_2_http_match',
+        'test_response_2_cert_match',
+        'test_response_2_asnum_match',
+        'test_response_2_asname_match',
+        'test_response_2_match_percentage',
+        'test_response_2_asnum',
+        'test_response_3_IP_match',
+        'test_response_3_http_match',
+        'test_response_3_cert_match',
+        'test_response_3_asnum_match',
+        'test_response_3_asname_match',
+        'test_response_3_match_percentage',
+        'test_response_3_asnum',
+        'test_response_4_IP_match',
+        'test_response_4_http_match',
+        'test_response_4_cert_match',
+        'test_response_4_asnum_match',
+        'test_response_4_asname_match',
+        'test_response_4_match_percentage',
+        'test_response_4_asnum',
+    ]
+
+    df = df[df.columns.intersection(columns_to_keep)] # drop unwanted columns
+
+    # Create new boolean columns to indicate more IPs
+    row_count = df.shape[0]
+
+    ip_count = df['test_response_IP_count']
+
+    more_ips = np.full(shape=row_count, dtype=bool, fill_value=False)
+
+    for index in range(0, row_count):
+        if ip_count.iloc[index] > 5:
+            more_ips[index] = True
+
+    df['more_IPs'] = pd.Series(more_ips)
+
+    #  Create new boolean columns to indicate if IP is being used
+
+    include_IP_0 = np.full(shape=row_count, dtype=bool, fill_value=False)
+    include_IP_1 = np.full(shape=row_count, dtype=bool, fill_value=False)
+    include_IP_2 = np.full(shape=row_count, dtype=bool, fill_value=False)
+    include_IP_3 = np.full(shape=row_count, dtype=bool, fill_value=False)
+    include_IP_4 = np.full(shape=row_count, dtype=bool, fill_value=False)
+
+    for index in range(0, row_count):
+        if ip_count.iloc[index] == 1:
+            include_IP_0[index] = True
+
+        if ip_count.iloc[index] == 2:
+            include_IP_1[index] = True
+
+        if ip_count.iloc[index] == 3:
+            include_IP_2[index] = True
+
+        if ip_count.iloc[index] == 4:
+            include_IP_3[index] = True
+
+        if ip_count.iloc[index] == 5:
+            include_IP_4[index] = True
+
+    df['include_IP_0'] = pd.Series(include_IP_0)
+    df['include_IP_1'] = pd.Series(include_IP_1)
+    df['include_IP_2'] = pd.Series(include_IP_2)
+    df['include_IP_3'] = pd.Series(include_IP_3)
+    df['include_IP_4'] = pd.Series(include_IP_4)
+
+    # Change IP match percentage rate to same as average if IP is not being used
+
+    new_test_response_0_match_percentage = np.full(shape=row_count, dtype=np.float64(), fill_value=0)
+    new_test_response_1_match_percentage = np.full(shape=row_count, dtype=np.float64(), fill_value=0)
+    new_test_response_2_match_percentage = np.full(shape=row_count, dtype=np.float64(), fill_value=0)
+    new_test_response_3_match_percentage = np.full(shape=row_count, dtype=np.float64(), fill_value=0)
+    new_test_response_4_match_percentage = np.full(shape=row_count, dtype=np.float64(), fill_value=0)
+
+    old_test_response_0_match_percentage = df['test_response_0_match_percentage']
+    old_test_response_1_match_percentage = df['test_response_1_match_percentage']
+    old_test_response_2_match_percentage = df['test_response_2_match_percentage']
+    old_test_response_3_match_percentage = df['test_response_3_match_percentage']
+    old_test_response_4_match_percentage = df['test_response_4_match_percentage']
+
+    average_matchrate = df['average_matchrate']
+
+    for index in range(0, row_count):
+        if old_test_response_0_match_percentage.iloc[index] < 0:  # if empty IP, should be -2 in float notation if
+            new_test_response_0_match_percentage[index] = average_matchrate[index]
+
+        if old_test_response_1_match_percentage.iloc[index] < 0:  # if empty IP, should be -2 in float notation if
+            new_test_response_1_match_percentage[index] = average_matchrate[index]
+
+        if old_test_response_2_match_percentage.iloc[index] < 0:  # if empty IP, should be -2 in float notation if
+            new_test_response_2_match_percentage[index] = average_matchrate[index]
+
+        if old_test_response_3_match_percentage.iloc[index] < 0:  # if empty IP, should be -2 in float notation if
+            new_test_response_3_match_percentage[index] = average_matchrate[index]
+
+        if old_test_response_4_match_percentage.iloc[index] < 0:  # if empty IP, should be -2 in float notation if
+            new_test_response_4_match_percentage[index] = average_matchrate[index]
+
+    df['test_response_0_match_percentage'] = pd.Series(new_test_response_0_match_percentage)
+    df['test_response_1_match_percentage'] = pd.Series(new_test_response_1_match_percentage)
+    df['test_response_2_match_percentage'] = pd.Series(new_test_response_2_match_percentage)
+    df['test_response_3_match_percentage'] = pd.Series(new_test_response_3_match_percentage)
+    df['test_response_4_match_percentage'] = pd.Series(new_test_response_4_match_percentage)
+
+    # One-hot encoding of discrete variables, including booleans (be sure to remove old features)
+
+    categorical_columns = [
+        'untagged_controls',
+        'untagged_response',
+        'passed_liveness',
+        'connect_error',
+        'in_control_group',
+        'excluded',
+        'excluded_is_CDN',
+        'excluded_below_threshold',
+        'control_response_start_success',
+        'control_response_end_success',
+        'control_response_start_has_type_a',
+        'control_response_start_rcode',
+        'control_response_end_has_type_a',
+        'control_response_end_rcode',
+        'test_query_successful',
+        'test_noresponse_1_has_type_a',
+        'test_noresponse_1_rcode',
+        'test_noresponse_2_has_type_a',
+        'test_noresponse_2_rcode',
+        'test_noresponse_3_has_type_a',
+        'test_noresponse_3_rcode',
+        'test_noresponse_4_has_type_a',
+        'test_noresponse_4_rcode',
+        'test_response_has_type_a',
+        'test_response_rcode',
+        'more_IPs',
+        'test_response_0_IP_match',
+        'test_response_0_http_match',
+        'test_response_0_cert_match',
+        'test_response_0_asnum_match',
+        'test_response_0_asname_match',
+        'test_response_0_asnum',
+        'include_IP_0',
+        'test_response_1_IP_match',
+        'test_response_1_http_match',
+        'test_response_1_cert_match',
+        'test_response_1_asnum_match',
+        'test_response_1_asname_match',
+        'test_response_1_asnum',
+        'include_IP_1',
+        'test_response_2_IP_match',
+        'test_response_2_http_match',
+        'test_response_2_cert_match',
+        'test_response_2_asnum_match',
+        'test_response_2_asname_match',
+        'test_response_2_asnum',
+        'include_IP_2',
+        'test_response_3_IP_match',
+        'test_response_3_http_match',
+        'test_response_3_cert_match',
+        'test_response_3_asnum_match',
+        'test_response_3_asname_match',
+        'test_response_3_asnum',
+        'include_IP_3',
+        'test_response_4_IP_match',
+        'test_response_4_http_match',
+        'test_response_4_cert_match',
+        'test_response_4_asnum_match',
+        'test_response_4_asname_match',
+        'test_response_4_asnum',
+        'include_IP_4',
+    ]
+
+    for column in categorical_columns:
+        temp_df = pd.get_dummies(df[column], prefix=column)
+
+        df = pd.merge(left=df, right=temp_df, left_index=True, right_index=True)
+
+        df = df.drop(columns=column)
+
+    # Standard scale all variables
+    # Note that all series metadata is lost at this point
+
+    column_list = df.columns.values.tolist()
+
+    numpy_df = df.to_numpy()
+
+    scaler = StandardScaler()
+
+    scaler = scaler.fit(numpy_df)
+
+    numpy_df = scaler.transform(numpy_df)
+
+    df = pd.DataFrame(numpy_df, columns=column_list)
+
+    return df
---
+++ Convert_To_ML_Sets.py
@@ -0,0 +1,241 @@
+"""
+ Divide the aggregate dataframes into train, validation, and test sets.
+ All invalid records are dropped and two copies of the validation and test sets are made
+ One copy has both the unclean and clean (presumed uncensored) data, and the other only the clean data
+ """
+
+import pandas as pd
+import os
+from Convert_To_ML_Helper_Methods import *
+from add_GFWatch_column import run_add_GFWatch_column
+
+
+# remove invalid records
+def remove_invalid_records(df):
+
+    valid_df = df[(df.in_control_group == True) & (df.control_response_start_success == True) & (df.excluded_below_threshold == False)]
+
+    # TODO create copy of the table
+
+    return valid_df.copy()
+
+def get_ASN_list(input_df, row):
+
+    IP_count = input_df['test_response_IP_count'].iloc[row]
+
+    asn_list = []
+
+    for index in range(0, IP_count):
+
+        asn_list.append(input_df['test_response_' +str(index)+ '_asnum'].iloc[row])
+
+    return asn_list
+
+def remove_unclean_records (input_df, AS_count_df):
+
+    # TODO complete this function
+
+    # Convert AS_count_df into dictionary
+
+    domain_datetime_clean_dict = {}
+    domain_datetime_asn_dict = {}
+
+    for row in range(0, AS_count_df.shape[0]):
+
+        datetime = AS_count_df['datetime'].iloc[row].strftime('%Y-%m-%d-%H-%M-%S')
+        domain = AS_count_df['domain_name'].iloc[row]
+        clean_records = AS_count_df['clean_records'].iloc[row]
+        asn = AS_count_df['most_common_asn_num'].iloc[row]
+
+        if domain not in domain_datetime_clean_dict.keys():
+
+            domain_datetime_clean_dict[domain] = {datetime: clean_records}
+            domain_datetime_asn_dict[domain] = {datetime: asn}
+
+        else:
+
+            domain_datetime_clean_dict[domain][datetime] = clean_records
+            domain_datetime_asn_dict[domain][datetime] = asn
+
+    indices_to_remove = []
+
+    for row in range(0, input_df.shape[0]):
+
+        datetime = input_df['batch_datetime'].iloc[row].strftime('%Y-%m-%d-%H-%M-%S')
+        domain = input_df['test_url'].iloc[row]
+        control_response_end_success = input_df['control_response_end_success'].iloc[row]
+        anomaly = input_df['anomaly'].iloc[row]
+        connect_error = input_df['connect_error'].iloc[row]
+        test_query_successful = input_df['test_query_successful'].iloc[row]
+
+        remove_index = True
+
+        asn_list = get_ASN_list(input_df, row)
+
+        # Check if domain and datetime combination is in the ASN list
+        if domain in domain_datetime_clean_dict.keys() and datetime in domain_datetime_clean_dict[domain].keys():
+
+            # Check ASN list quality control
+            if domain_datetime_clean_dict[domain][datetime] == "Clean":
+
+                if control_response_end_success == True and anomaly == False and connect_error == False and test_query_successful == True:
+
+                    if domain_datetime_asn_dict[domain][datetime] in asn_list:
+
+                        remove_index = False
+
+        if remove_index == True:
+
+            indices_to_remove.append(row)
+
+    clean_df = input_df.drop(index=indices_to_remove)
+
+    return clean_df.copy()
+
+# Create the training data from the original dataframe
+def create_ML_ready_data(df, AS_count_df, clean_only, index_list, save_filename, data_type):
+
+    df = df[index_list[0], index_list[1]].copy() # Select only the rows we want
+
+    total_records_count = df.shape[0]
+
+    if clean_only == False:
+
+        clean_moniker = "Mixed"
+
+    if clean_only == True:
+
+        df = remove_unclean_records(df, AS_count_df)
+
+        clean_moniker = "Clean"
+
+    records_removed_count = total_records_count - df.shape[0]
+
+    df['anomaly'].astype(int).to_csv(path_or_buf=save_filename+data_type+ "_" +clean_moniker+ "_targetFeature_anomaly.csv", \
+             index=False) # Converts True and False to 1 and 0
+
+    df.drop(['anomaly'], axis=1)
+
+    if country_code == 'CN':
+
+        df['GFWatch_Censored'].to_csv(path_or_buf=save_filename+data_type+ "_" +clean_moniker+ "_targetFeature_GFWatch_Censored.csv", \
+                index=False)
+
+        df.drop(['GFWatch_Censored'], axis=1)
+
+    elif country_code == "US":
+
+        df['Presumed_Censored'].to_csv(path_or_buf=save_filename+data_type+ "_" +clean_moniker+ "_targetFeature_Presumed_Censored.csv", \
+                index=False)
+
+        df.drop(['Presumed_Censored'], axis=1)
+
+    else:
+
+        pass # No presumption of censorship for other countries
+
+    df = create_ML_features(df)
+
+    df.to_parquet(index=True, compression="gzip", engine='pyarrow',  path=save_filename+data_type+ "_" +clean_moniker+ "_descriptiveFeatures_fullDataset.gzip")
+
+    return total_records_count, records_removed_count
+
+country_code = "CN"
+country_name = "China"
+
+training_split_fraction = 0.8
+validation_split_fraction = 0.1
+testing_split_fraction = 1 - training_split_fraction - validation_split_fraction
+
+home_file_name = r"/home/jambrown/CP_Analysis/"
+ml_ready_data_file_name = home_file_name +country_code+ "/ML_ready_dataframes/"
+os.mkdir(ml_ready_data_file_name)
+aggregate_file_name = home_file_name +country_code+ "/raw_dataframe.gzip"
+
+GFWatch_table_filename = r'/home/jambrown/CP_Analysis/gfwatch_censored_domains.csv'
+AS_count_table_filename = home_file_name + "max_asn_aggregate.gzip"
+
+AS_count_df = pd.read_parquet(path=AS_count_table_filename, engine='pyarrow')
+
+original_df = pd.read_parquet(path=aggregate_file_name, engine='pyarrow')
+
+original_df_length = original_df.shape[0]
+
+valid_df = remove_invalid_records(original_df)
+
+valid_df_length = valid_df.shape[0]
+
+print("Total number of probes: " +str(original_df_length))
+print("Valid number of probes: " +str(valid_df_length))
+
+# Calculate indices for dividing data into unique training, validation, and testing components
+training_index_list = (0, int(valid_df_length*training_split_fraction))
+validation_index_list = (int(valid_df_length*training_split_fraction)+1, int(valid_df_length*(training_split_fraction+validation_split_fraction)))
+testing_index_list = (int(valid_df_length*(training_split_fraction+validation_split_fraction))+1, valid_df_length)
+
+if country_code == "CN":
+
+    original_with_newColumn_df = run_add_GFWatch_column(valid_df, GFWatch_table_filename)
+
+elif country_code == "US":
+
+    original_with_newColumn_df = valid_df # change the reference
+    original_with_newColumn_df['Presumed_Censored'] = 0  # All US records are presumed uncensored
+
+else:
+
+    original_with_newColumn_df = valid_df  # change the reference and don't add the new column
+
+original_with_newColumn_df = original_with_newColumn_df.sample(frac=1) # Shuffle the dataframe - this is the last time this is done in the pipeline
+
+# TODO uncomment this section
+# # Create clean training dataset
+# total_records_count, records_removed_count \
+#     = create_ML_ready_data(original_with_newColumn_df, AS_count_df, clean_only=True,
+#                            index_list=training_index_list, save_filename=ml_ready_data_file_name, data_type="TRAINING")
+#
+# print("Training data (clean) total probes: " +str(total_records_count))
+# print("Training data (clean) probes removed: " +str(records_removed_count))
+#
+# # Create mixed training set
+# total_records_count, records_removed_count \
+#     = create_ML_ready_data(original_with_newColumn_df, AS_count_df, clean_only=False,
+#                            index_list=training_index_list, save_filename=ml_ready_data_file_name, data_type="TRAINING")
+#
+# print("Training data (mixed) total probes: " +str(total_records_count))
+# print("Training data (mixed) probes removed: " +str(records_removed_count))
+#
+# # Create clean validation dataset
+# total_records_count, records_removed_count \
+#     = create_ML_ready_data(original_with_newColumn_df, AS_count_df, clean_only=True,
+#                            index_list=validation_index_list, save_filename=ml_ready_data_file_name, data_type="VALIDATION")
+#
+# print("Validation data (clean) total probes: " +str(total_records_count))
+# print("Validation data (clean) probes removed: " +str(records_removed_count))
+#
+# # Create mixed validation set
+# total_records_count, records_removed_count \
+#     = create_ML_ready_data(original_with_newColumn_df, AS_count_df, clean_only=False,
+#                            index_list=validation_index_list, save_filename=ml_ready_data_file_name, data_type="VALIDATION")
+#
+# print("Validation data (mixed) total probes: " +str(total_records_count))
+# print("Validation data (mixed) probes removed: " +str(records_removed_count))
+
+# Create clean testing dataset
+total_records_count, records_removed_count \
+    = create_ML_ready_data(original_with_newColumn_df, AS_count_df, clean_only=True,
+                           index_list=testing_index_list, save_filename=ml_ready_data_file_name, data_type="TESTING")
+
+print("Testing data (clean) total probes: " +str(total_records_count))
+print("Testing data (clean) probes removed: " +str(records_removed_count))
+
+# Create mixed testing set
+total_records_count, records_removed_count \
+    = create_ML_ready_data(original_with_newColumn_df, AS_count_df, clean_only=False,
+                           index_list=testing_index_list, save_filename=ml_ready_data_file_name, data_type="TESTING")
+
+print("Testing data (mixed) total probes: " +str(total_records_count))
+print("Testing data (mixed) probes removed: " +str(records_removed_count))
+
+
+
